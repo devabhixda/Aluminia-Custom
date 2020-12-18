e513958c043daedcb08d84464a4109bcad83106a
Abhi Jain <devabhixda@gmail.com>
2020-12-18 14:29:10 +0530

    Add life to home

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,23 +1,37 @@
+import 'package:aluminia/AddPost.dart';
+import 'package:aluminia/Models/User.dart';
+import 'package:aluminia/PostDetail.dart';
+import 'package:aluminia/Profile.dart';
+import 'package:aluminia/Services/auth.dart';
 import 'package:aluminia/const.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
+  final String userName;
+  Home({this.userName});
   @override
   _HomeState createState() => _HomeState();
 }
 class _HomeState extends State<Home> {
   double w,h;
+  String comment;
+  Auth auth = new Auth();
+  List<Post> posts;
+  bool commentBox = false, loading = true;
 
   void initState() {
     super.initState();
-    setLogin();
+    init();
   }
 
-  setLogin() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool('login', true);
+  init() async {
+    List<Post> temp = await auth.getPosts();
+    setState(() {
+      posts = temp;
+      loading = false;
+    });
   }
 
   @override    
@@ -28,7 +42,7 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text(
           "Aluminia",
-          style: GoogleFonts.comfortaa(
+          style: GoogleFonts.poppins(
             color: blu,
             fontSize: 32
           ),  
@@ -36,9 +50,14 @@ class _HomeState extends State<Home> {
         actions: [
           Padding(
             padding: EdgeInsets.only(right: 0.05 * w),
-            child: CircleAvatar(
-              child: Icon(Icons.person),
-              backgroundColor: blu,
+            child: GestureDetector(
+              child: CircleAvatar(
+                child: Icon(Icons.person),
+                backgroundColor: blu,
+              ),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Profile(userName: widget.userName,)));
+              },
             )
           )
         ],
@@ -46,48 +65,54 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: SingleChildScrollView(
+      body: loading ? SpinKitDoubleBounce(
+        color: blu,
+      ) : SingleChildScrollView(
         child: Column(
           children: [
             Container(
               padding: EdgeInsets.only(bottom: 0.1 * h),
-              height: h,
+              height: 0.95 * h,
               child: ListView.builder(
-              itemCount: 10,
+              itemCount: posts.length,
               itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 0.8 * w,
-                        height: 0.2 * h,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)
-                          ),
-                          elevation: 5,
-                          color: blu,
-                        )
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.thumb_up_alt_outlined),
-                            Text(" Like"),
-                            SizedBox(
-                              width: 0.2 * w,
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => PostDetail(userName: widget.userName, id: posts[index].id, imgUrl: posts[index].imgUrl, content: posts[index].content)));
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 0.8 * w,
+                          height: 0.2 * h,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)
                             ),
-                            Icon(Icons.share_outlined),
-                            Text(" Share")
-                          ],
-                        )
-                      )
-                    ],
-                  ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image(image: NetworkImage(posts[index].imgUrl), fit: BoxFit.fill)
+                            ),
+                          )
+                        ),
+                        Container(
+                          width: 0.8 * w,
+                          child: Card(
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                              child: Text(posts[index].content)
+                            )
+                          )
+                        ),
+                      ],
+                    ),
+                  )
                 );
               }
             )
@@ -97,6 +122,9 @@ class _HomeState extends State<Home> {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => AddPost(userName: widget.userName,)));
+        },
       ),
     );
   }
