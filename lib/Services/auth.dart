d0ff810a6d0976a12a0a7ff12767a5256d32ffa6
Abhi Jain <devabhixda@gmail.com>
2020-12-18 14:26:43 +0530

    Drop Firestore in favour of Custom API
    Also store photos on firebase storage as imgur has limited API usage

--- lib/Services/auth.dart
+++ lib/Services/auth.dart
@@ -1,83 +1,229 @@
 import 'dart:io';
-import 'package:aluminia/client_secrets.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:aluminia/Models/User.dart';
+import 'package:aluminia/Screens/OnBoarding/Education.dart' as Edu;
+import 'package:aluminia/const.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:imgur/imgur.dart' as imgur;
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Auth{
-  FirebaseAuth auth = FirebaseAuth.instance;
-  Stream<User> get authStateChanges => auth.authStateChanges();
-  UserCredential userCredential;
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
-  CollectionReference users = FirebaseFirestore.instance.collection('users');
-  final client = imgur.Imgur(imgur.Authentication.fromToken(imgurAuth));
-
-  createAccount(String email, String password) async {
-    try {
-      userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password
-      );
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      prefs.setString('email', email);
-      await userCredential.user.sendEmailVerification();
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  Future<bool> signIn(String email, String password) async {
-    try {
-       userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password
-      );
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      prefs.setString('email', email);
-    } catch (e) {
-      print(e);
-    }
-    return userCredential != null;
-  }
-
-  Future<void> addUser(String name, String dob, String gender, File _imageFile, String phone) async{
+  createAccount(BuildContext context, user) async {
+    String url = server + "/User";
+    Response response = await post(Uri.encodeFull(url), 
+      body: json.encode(user), 
+      headers: {
+        "Content-Type": "application/json",
+        "Connection": "Keep-Alive"
+      },
+    );
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    String email = prefs.getString('email');
-    String image = await uploadImage(_imageFile);
-    return users.add({
-      'name': name,
-      'email': email,
-      'dob': dob,
-      'gender': gender,
-      'image': image,
-      'phone': phone
-    })
-    .then((value) => print("User Added"))
-    .catchError((error) => print("Failed to add user: $error"));
+    await prefs.setString("user_name", user["user_name"]);
+    await prefs.setString("pfp", user["Profile_Image_URL"]);
+    await prefs.setString("name", user["Name"]);
+    await prefs.setBool('login', true);
+    if(response.statusCode == 200) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Edu.Education(userName: user["user_name"])));
+    }
   }
+
+  addEducation(education) async {
+    print(education);
+    String url = server + "/Education";
+    Response response = await post(Uri.encodeFull(url), 
+      body: json.encode(education), 
+      headers: {
+        "Content-Type": "application/json",
+        "Connection": "Keep-Alive"
+      },
+    );
+  }
+
+  Future<bool> signIn(String user_name, String password) async {
+    String url = server + "/User" + "/$user_name";
+    Response response = await get(Uri.encodeFull(url));
+    var data = json.decode(response.body);
+    if(data[0]["Password"] == password) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString("user_name", user_name);
+      await prefs.setString("pfp", data[0]["Profile_Image_URL"]);
+      await prefs.setString("name", data[0]["Name"]);
+      await prefs.setBool('login', true);
+      return true;
+    }
+    return false;
+  }
+
 
   Future<String> uploadImage(File _imageFile) async {
-    String url = "";
-    await client.image
-      .uploadImage(
-          imagePath: _imageFile.path,
-          title: 'A title',
-          description: 'A description'
-        )
-      .then((image) => url = image.link);
-    return url;
+    print(_imageFile.hashCode);
+    final ref = FirebaseStorage.instance
+        .ref()
+        .child('posts')
+        .child(_imageFile.hashCode.toString());
+    if (_imageFile != null) {
+      await ref.putFile(_imageFile);
+      return await ref.getDownloadURL();
+    } 
+    return "";
   }
 
-  Future<bool> getUser(String email) async {
-    bool existing;
-    await users.where('email', isEqualTo: email).get().then((value) => value.size == 0 ? existing = false : existing = true);
-    return existing;
+  getUser(String user_name) async {
+    String url = server + "/User" + "/$user_name";
+    Response response = await get(Uri.encodeFull(url));
+    var data = json.decode(response.body);
+  }
+
+  addWorkEx(workex) async {
+    String url = server + "/Work_Experience";
+    Response response = await post(Uri.encodeFull(url), 
+      body: json.encode(workex), 
+      headers: {
+        "Content-Type": "application/json",
+        "Connection": "Keep-Alive"
+      },
+    );
+  }
+
+  Future<List<Education>> getEducation(String user) async {
+    String url = server + "/Education" + "/$user";
+    Response response = await get(Uri.encodeFull(url));
+    var data = json.decode(response.body);
+    List<Education> lst = [];
+    for(int i=0;i<data.length;i++) {
+      Education e = new Education(school: data[i]["School_Name"], degree: data[i]["Degree"], duration: data[i]["Duration"], percentage: data[i]["Percentage"]);
+      lst.add(e);
+    }
+    return lst;
+  }
+
+  Future<List<WorkEx>> geWorkEx(String user) async {
+    String url = server + "/Work_Experience" + "/$user";
+    Response response = await get(Uri.encodeFull(url));
+    var data = json.decode(response.body);
+    List<WorkEx> lst = [];
+    for(int i=0;i<data.length;i++) {
+      WorkEx e = new WorkEx(company: data[i]["Company"], designation: data[i]["Designation"], duration: data[i]["Duration"]);
+      lst.add(e);
+    }
+    return lst;
+  }
+
+  createPost(BuildContext context, postcon) async {
+    String url = server + "/Post";
+    Response response = await post(Uri.encodeFull(url), 
+      body: json.encode(postcon), 
+      headers: {
+        "Content-Type": "application/json",
+        "Connection": "Keep-Alive"
+      },
+    );
+    if(response.statusCode == 200) {
+      Fluttertoast.showToast(
+          msg: "Post added successfully",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: blu,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+      Navigator.pop(context);
+    }
+  }
+
+  Future<List<Post>> getPosts() async {
+    String url = server + "/Post";
+    Response response = await get(Uri.encodeFull(url));
+    var data = json.decode(response.body);
+    List<Post> lst = [];
+    for(int i=0;i<data.length;i++) {
+      Post e = new Post(id: data[i]["Post_Id"], imgUrl: data[i]["Picture_URL"], content: data[i]["Content"], timeStamp: data[i]["Timestamp"]);
+      lst.add(e);
+    }
+    return lst;
+  }
+
+  addLike(like) async {
+    String url = server + "/Likes";
+    Response response = await post(Uri.encodeFull(url), 
+      body: json.encode(like), 
+      headers: {
+        "Content-Type": "application/json",
+        "Connection": "Keep-Alive"
+      },
+    );
+  }
+
+  addComment(comment) async {
+    String url = server + "/Comment";
+    Response response = await post(Uri.encodeFull(url), 
+      body: json.encode(comment), 
+      headers: {
+        "Content-Type": "application/json",
+        "Connection": "Keep-Alive"
+      },
+    );
+    if(response.statusCode == 200) {
+      Fluttertoast.showToast(
+          msg: "Comment added successfully",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: blu,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+    }
+  }
+
+  Future<List<Comment>> getComments(int id) async {
+    String url = server + "/Comment/$id";
+    Response response = await get(Uri.encodeFull(url));
+    var data = json.decode(response.body);
+    List<Comment> lst = [];
+    for(int i=0;i<data.length;i++) {
+      Comment e = new Comment(content: data[i]["Content"], userName: data[i]["user_name"], timeStamp: data[i]["Timestamp"]);
+      lst.add(e);
+    }
+    return lst;
+  }
+
+  Future<List<Job>> getJobs() async {
+    String url = server + "/Job";
+    Response response = await get(Uri.encodeFull(url));
+    var data = json.decode(response.body);
+    List<Job> lst = [];
+    for(int i=0;i<data.length;i++) {
+      Job e = new Job(company: data[i]["Company"],description: data[i]["Description"], timeStamp: data[i]["Timestamp"], userName: data[i]["user_name"]);
+      lst.add(e);
+    }
+    return lst;
+  }
+
+  createJob(BuildContext context, job) async {
+    String url = server + "/Job";
+    Response response = await post(Uri.encodeFull(url), 
+      body: json.encode(job), 
+      headers: {
+        "Content-Type": "application/json",
+        "Connection": "Keep-Alive"
+      },
+    );
+    if(response.statusCode == 200) {
+      Fluttertoast.showToast(
+          msg: "Job added successfully",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: blu,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+      Navigator.pop(context);
+    }
   }
 }
